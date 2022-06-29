7143530b2e94624b7d1a8ef656b47d3955f584fe
s269152 <s269152@studenti.polito.it>
2022-06-29 09:41:08 +0200

    Versione Finale

--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -1,16 +1,98 @@
 package it.polito.tdp.nobel.model;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-public class Model {
+import it.polito.tdp.nobel.db.EsameDAO;
 
+public class Model {
 	
-	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
-		System.out.println("TODO!");
-		return null;	
+	private List<Esame> esami; 
+	private Set<Esame> migliore;
+	private double mediaMigliore;
+	
+	
+	public Model() {
+		EsameDAO dao= new EsameDAO(); 
+		this.esami = dao.getTuttiEsami();
+	}
+
+
+	public Set<Esame> calcolaSottoinsiemeEsami(int m) {
+		migliore= new HashSet<Esame>(); 
+		mediaMigliore=0; 
+		
+		Set<Esame> parziale= new HashSet<Esame>(); 
+		//cerca1(parziale,0,m); 
+		cerca2(parziale,0,m);
+		
+		return migliore;	
 	}
 
 	
+	private void cerca2(Set<Esame> parziale, int L, int m) {
+		//Controllare i casi terminali
+		int sommaCrediti= sommaCrediti(parziale); 
+		if (sommaCrediti>m)
+			return; //soluzione non valida
+		if (sommaCrediti==m) {
+			//soluzione valida, controlliamo se è la migliore fino a qui
+			double mediaVoti=calcolaMedia(parziale); 
+			if(mediaVoti>mediaMigliore) {
+				migliore=new HashSet<Esame>(parziale);//non voglio il riferimento ma una copia
+				mediaMigliore=mediaVoti; 
+			}
+			return; 
+		}
+		
+		//sicuramente i crediti sono minori di m 
+		if(L==esami.size())//abbiamo terminato gli esami da inserire
+			return;		
+		
+		//generiamo i sotto-problemi
+		//provo ad aggiungere esami[L]
+		parziale.add(esami.get(L)); 
+		cerca2(parziale, L+1, m); 
+		
+		//provo a non aggiungere esami[L]
+		parziale.remove(esami.get(L)); 
+		cerca2(parziale,L+1,m);
+	}
+
+
+	private void cerca1(Set<Esame> parziale, int L, int m) {
+		//Controllare i casi terminali
+		int sommaCrediti= sommaCrediti(parziale); 
+		if (sommaCrediti>m)
+			return; //soluzione non valida
+		if (sommaCrediti==m) {
+			//soluzione valida, controlliamo se è la migliore fino a qui
+			double mediaVoti=calcolaMedia(parziale); 
+			if(mediaVoti>mediaMigliore) {
+				migliore=new HashSet<Esame>(parziale);//non voglio il riferimento ma una copia
+				mediaMigliore=mediaVoti; 
+			}
+			return; 
+		}
+		
+		//sicuramente i crediti sono minori di m 
+		if(L==esami.size())//abbiamo terminato gli esami da inserire
+			return; 
+		
+		//generiamo i sotto-problemi
+		for(Esame e: esami) {
+			if(!parziale.contains(e)) {
+				parziale.add(e);
+				cerca1(parziale, L+1, m); 
+				parziale.remove(e); //i set non contengono duplicati
+									//altrimenti con una lista
+									// avremmo fatto parziale.remove(parziale.size()-1)
+			}
+		}
+	}
+
+
 	public double calcolaMedia(Set<Esame> esami) {
 		
 		int crediti = 0;
